b0b60e01c6a6be0580e601eeb4a29eb0000530f9
纹枰老妖 <296646069@qq.com>
2017-12-05 15:14:52 +0800

    更新 tank.cpp

--- tank.cpp
+++ tank.cpp
@@ -1,6 +1,6 @@
 #include "tank.h"
 
-Tank::Tank(int vertical,int horizon,int dir,int sp,int hp,bool f)
+Tank::Tank(int vertical, int horizon, int dir, int sp, int hp, bool f)
 {
 	HP = hp;
 	verticalTop = vertical;
@@ -16,8 +16,8 @@ Tank::~Tank()
 
 int Tank::Hurt(int verT, int horiT, int power)  //检测坦克是否受伤，其中传递过来的参数应该是炮弹的位置（由前两个参数表示），第三个参数是炮弹的威力
 {
-	if (horizonTop < horiT 
-		&& verticalTop < verT 
+	if (horizonTop < horiT
+		&& verticalTop < verT
 		&& horizonTop + LENGTH > horiT
 		&& verticalTop + LENGTH > verT)
 		return power; //如果受伤了，就返回受伤值
@@ -26,12 +26,12 @@ int Tank::Hurt(int verT, int horiT, int power)  //检测坦克是否受伤，其
 
 bool Tank::BeCollide(int vertical, int horizon) //检测坦克是否撞到别的坦克，两个参数表示另外坦克的位置
 {
-	if (verticalTop+1<vertical+LENGTH && horizonTop+1<horizon+LENGTH && verticalTop + LENGTH>vertical+1 && horizonTop + LENGTH>horizon+1)
+	if (verticalTop + 1<vertical + LENGTH && horizonTop + 1<horizon + LENGTH && verticalTop + LENGTH>vertical + 1 && horizonTop + LENGTH>horizon + 1)
 		return true;
 	else return false;
 }
 
-Bullet::Bullet(int ver,int hor,int sp, int dir, int pow)
+Bullet::Bullet(int ver, int hor, int sp, int dir, int pow)
 {
 	verticalTop = ver;
 	horizonTop = hor;
@@ -40,7 +40,7 @@ Bullet::Bullet(int ver,int hor,int sp, int dir, int pow)
 	power = pow;
 }
 
-Obstacle::Obstacle(int ver, int hori, int hp):vertical(ver),horizon(hori),HP(hp)
+Obstacle::Obstacle(int ver, int hori, int hp) :vertical(ver), horizon(hori), HP(hp)
 {}
 bool Obstacle::Hurt(int ver, int hori) //检测障碍物是否受伤
 {
@@ -48,6 +48,13 @@ bool Obstacle::Hurt(int ver, int hori) //检测障碍物是否受伤
 		&& vertical <= ver
 		&& horizon + LENGTH >= hori
 		&& vertical + LENGTH >= ver)
+		return true;
+	else return false;
+}
+
+bool Obstacle::BeCollide(int v, int h)//检测障碍物是否被撞到
+{
+	if (vertical + 1<vertical + LENGTH && horizon + 1<h + LENGTH && vertical + LENGTH>v + 1 && horizon + LENGTH>h + 1)
 		return true;
 	else return false;
 }
