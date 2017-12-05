7584a461b531ecbd505c517c60b319c0f07155fb
纹枰老妖 <296646069@qq.com>
2017-12-05 15:23:22 +0800

    更新 源.cpp

--- 源.cpp
+++ 源.cpp
@@ -12,18 +12,18 @@ Game* game = NULL;
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-	WNDCLASSEX wndClass = { 0 };							//用WINDCLASSEX定义了一个窗口类
-	wndClass.cbSize = sizeof(WNDCLASSEX);			//设置结构体的字节数大小
+	WNDCLASSEX wndClass = { 0 }; //用WINDCLASSEX定义了一个窗口类
+	wndClass.cbSize = sizeof(WNDCLASSEX); //设置结构体的字节数大小
 	wndClass.style = CS_HREDRAW | CS_VREDRAW;	//设置窗口的样式
-	wndClass.lpfnWndProc = WndProc;					//设置指向窗口过程函数的指针
-	wndClass.cbClsExtra = 0;								//窗口类的附加内存，取0就可以了
-	wndClass.cbWndExtra = 0;							//窗口的附加内存，依然取0就行了
-	wndClass.hInstance = hInstance;						//指定包含窗口过程的程序的实例句柄。
-	wndClass.hIcon = (HICON)::LoadImage(NULL, L"icon.ico", IMAGE_ICON, 0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);  //本地加载自定义ico图标
-	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);    //指定窗口类的光标句柄。
+	wndClass.lpfnWndProc = WndProc;	//设置指向窗口过程函数的指针
+	wndClass.cbClsExtra = 0; //窗口类的附加内存，取0就可以了
+	wndClass.cbWndExtra = 0; //窗口的附加内存，依然取0就行了
+	wndClass.hInstance = hInstance;	 //指定包含窗口过程的程序的实例句柄。
+	wndClass.hIcon = (HICON)::LoadImage(NULL, L"icon.ico", IMAGE_ICON, 0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE); //本地加载自定义ico图标
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW); //指定窗口类的光标句柄。
 	wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);  //为hbrBackground成员指定一个白色画刷句柄	
-	wndClass.lpszMenuName = NULL;						//设置菜单资源为空。
-	wndClass.lpszClassName = L"机甲传说，作者by范效萌";		//用一个以空终止的字符串，指定窗口类的名字。
+	wndClass.lpszMenuName = NULL; //设置菜单资源为空。
+	wndClass.lpszClassName = L"坦克小战，作者：范效萌"; //用一个以空终止的字符串，指定窗口类的名字。
 
 	if (!RegisterClassEx(&wndClass)) return -1;
 
@@ -34,7 +34,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 		WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, hInstance, NULL);
 
 	game = new Game(hWnd); //先初始化我们的游戏类对象
-	game->level = 8;  //做好后删除这一句代码！！这是测试所用
+	game->level = 0;  //做好后删除这一句代码！！这是测试所用
 	MoveWindow(hWnd, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, true);
 	ShowWindow(hWnd, nShowCmd);
 	UpdateWindow(hWnd);
@@ -72,13 +72,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_KEYDOWN: //接收键盘按下的消息并做相应处理
 		if (game->my != NULL)
 		{
-			if (wParam == VK_UP)    game->my->direction = 0,game->my->speed = game->speedTemp; //按下向上键时，恢复玩家坦克速度（与下面的松开按键配合），让坦克向上走，以下同理
-			if (wParam == VK_RIGHT) game->my->direction = 1,game->my->speed = game->speedTemp; //。。。
+			if (wParam == VK_UP)    game->my->direction = 0, game->my->speed = game->speedTemp; //按下向上键时，恢复玩家坦克速度（与下面的松开按键配合），让坦克向上走，以下同理
+			if (wParam == VK_RIGHT) game->my->direction = 1, game->my->speed = game->speedTemp; //。。。
 			if (wParam == VK_DOWN)  game->my->direction = 2, game->my->speed = game->speedTemp;//。。。
 			if (wParam == VK_LEFT)  game->my->direction = 3, game->my->speed = game->speedTemp;//。。。
 			if (wParam == VK_SPACE)   game->my->fire = true; //按下空格键之后，如果玩家坦克没有炮弹存在，那么发射一颗炮弹
 		}
-	  	if (wParam == VK_TAB) (game ->* (game->pFunInitLevel[game->level]))(); //这个用来初始化当前关卡（按下TAB键之后）
+		if (wParam == VK_TAB) (game ->* (game->pFunInitLevel[game->level]))(); //这个用来初始化当前关卡（按下TAB键之后）
 		break;
 	case WM_KEYUP: //接收按键的消息并做相应处理
 		if (game->my != NULL)
